1fa5c1b95ea01bd1d8c768d73cb4db913497b5bb
Rishabh Parmar <95970520+Rishabh3243@users.noreply.github.com>
2022-08-29 01:22:31 +0530

    bro

---
+++ index.js
@@ -0,0 +1,11 @@
+// <!-- typed js effect starts -->
+var typed = new Typed(".typ", {
+    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
+
+ 
